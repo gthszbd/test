8f39d105f793b198e327ae2437d1321b27a683bc
guotaihan <guotaihan@guotaihandeMac-mini.local>
2015-12-22 21:30:03 +0800

    ss

---
+++ Myname/Myname/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Myname
+//
+//  Created by guotaihan on 15/12/22.
+//  Copyright © 2015年 guotaihan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Myname/Myname/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Myname
+//
+//  Created by guotaihan on 15/12/22.
+//  Copyright © 2015年 guotaihan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
